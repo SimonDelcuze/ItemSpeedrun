3597f3d38b5e07c8d507772eaaf00230d1d6f76a
Simon Delcuze <simon.delcuze@epitech.eu>
2024-03-23 00:52:51 +0100

    [ADD] Setting up the player

--- Source Code/src/SetupWorld/Setup.java
+++ Source Code/src/SetupWorld/Setup.java
@@ -1,5 +1,6 @@
 package SetupWorld;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -7,27 +8,89 @@ import org.bukkit.entity.Player;
 import org.bukkit.WorldCreator;
 import java.util.Random;
 import org.bukkit.World;
+import org.bukkit.Location;
+import org.bukkit.block.Biome;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.Bukkit;
 
-public class Setup implements CommandExecutor {
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-            long seed = new Random().nextLong();
-            String worldName = "world_" + seed;
-            WorldCreator worldCreator = new WorldCreator(worldName);
-            worldCreator.seed(seed);
-            player.sendMessage("Création d'un nouveau monde");
-            World newWorld = player.getServer().createWorld(worldCreator);
-            if (newWorld != null) {
-                player.teleport(newWorld.getSpawnLocation());
-            } else {
-                player.sendMessage("Une erreur est survenue lors de la création du monde.");
+public class Setup implements CommandExecutor
+{
+
+    //Setup the player's inventory and health
+    public void setupPlayer(Player player)
+    {
+        player.getInventory().clear();
+        player.setHealth(20);
+        player.setFoodLevel(20);
+        player.setSaturation(20);
+        player.setGameMode(org.bukkit.GameMode.SURVIVAL);
+        player.getInventory().clear();
+        PlayerInventory inventory = player.getInventory();
+        ItemStack item = new ItemStack(Material.COOKED_BEEF, 64);
+        inventory.addItem(item);
+    }
+
+    //Build an invisible box around the player
+    public void encasePlayer(Player player)
+    {
+        World world = player.getWorld();
+        Location playerLocation = player.getLocation();
+        playerLocation.setX(playerLocation.getBlockX() + 0.5);
+        playerLocation.setY(playerLocation.getBlockY());
+        playerLocation.setZ(playerLocation.getBlockZ() + 0.5);
+        int radius = 1;
+
+        for (int x = -radius; x <= radius; x++) {
+            for (int y = 0; y <= 3; y++) {
+                for (int z = -radius; z <= radius; z++) {
+                    if ((y == 1 || y == 2) && x == 0 && z == 0) {
+                        continue;
+                    }
+                    Location blockLocation = playerLocation.clone().add(x, y, z);
+                    world.getBlockAt(blockLocation).setType(Material.BARRIER);
+                }
             }
-            return true;
-        } else {
+        }
+    }
+
+    // Load a new map for the player to play on
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
             sender.sendMessage("Cette commande ne peut être utilisée que par un joueur.");
             return false;
         }
+        Player player = (Player) sender;
+        World newWorld = null;
+        boolean acceptableBiome = false;
+        long seed;
+        WorldCreator worldCreator;
+        Biome biomeAtSpawn;
+        while (!acceptableBiome) {
+            seed = new Random().nextLong();
+            worldCreator = new WorldCreator("world_" + seed);
+            worldCreator.seed(seed);
+            newWorld = player.getServer().createWorld(worldCreator);
+            biomeAtSpawn = newWorld.getBiome(0, newWorld.getHighestBlockYAt(0, 0));
+            if (biomeAtSpawn != Biome.OCEAN && biomeAtSpawn != Biome.RIVER && biomeAtSpawn != Biome.DEEP_OCEAN) {
+                acceptableBiome = true;
+            } else {
+                player.getServer().unloadWorld(newWorld, false);
+            }
+        }
+        if (newWorld != null) {
+            Location spawnLocation = newWorld.getHighestBlockAt(newWorld.getSpawnLocation()).getLocation().add(0, 1, 0);
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                player.teleport(spawnLocation);
+                Location playerLocation = player.getLocation();
+                setupPlayer(p);
+                encasePlayer(p);
+                playerLocation.setX(playerLocation.getBlockX() + 0.5);
+                playerLocation.setY(playerLocation.getBlockY() + 1);
+                playerLocation.setZ(playerLocation.getBlockZ() + 0.5);
+                p.teleport(playerLocation);
+            }
+        }
+        return true;
     }
 }
