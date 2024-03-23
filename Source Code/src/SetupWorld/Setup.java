61d711c03952fede5c757e798844b688de250299
Simon Delcuze <simon.delcuze@epitech.eu>
2024-03-23 13:04:23 +0100

    [ADD] Loading screen when the world is loading

--- Source Code/src/SetupWorld/Setup.java
+++ Source Code/src/SetupWorld/Setup.java
@@ -16,6 +16,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
+import java.io.File;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Setup implements CommandExecutor
@@ -28,14 +29,12 @@ public class Setup implements CommandExecutor
         player.setHealth(20);
         player.setFoodLevel(20);
         player.setSaturation(20);
-        player.setGameMode(org.bukkit.GameMode.ADVENTURE);
         player.getInventory().clear();
         PlayerInventory inventory = player.getInventory();
         ItemStack item = new ItemStack(Material.COOKED_BEEF, 64);
         inventory.addItem(item);
     }
 
-    //Build an invisible box around the player
     public void encasePlayer(Player player, Material material)
     {
         World world = player.getWorld();
@@ -64,16 +63,34 @@ public class Setup implements CommandExecutor
     {
         new BukkitRunnable() {
             int counter = 5;
+            ChatColor[] colors = {
+                    ChatColor.RED,
+                    ChatColor.GOLD,
+                    ChatColor.YELLOW,
+                    ChatColor.GREEN,
+                    ChatColor.AQUA,
+                    ChatColor.BLUE,
+                    ChatColor.LIGHT_PURPLE
+            };
+
             public void run() {
                 if (counter > 0) {
-                    player.sendTitle("", ChatColor.LIGHT_PURPLE + "" + counter);
+                    ChatColor color = colors[counter % colors.length];
+                    player.sendTitle(color + "" + ChatColor.BOLD + counter, "");
                     counter--;
                 } else {
-                    player.sendTitle("", ChatColor.LIGHT_PURPLE + "Go!");
+                    ChatColor color = colors[0];
+                    player.sendTitle(color + "" + ChatColor.BOLD + "Go!", "");
+                    new BukkitRunnable() {
+                        @Override
+                        public void run() {
+                            player.sendTitle("", "");
+                        }
+                    }.runTaskLater(Bukkit.getPluginManager().getPlugin("plugin"), 20L);
                     cancel();
                 }
             }
-        }.runTaskTimer(Bukkit.getPluginManager().getPlugin("plugin"), 10, 20L);
+        }.runTaskTimer(Bukkit.getPluginManager().getPlugin("plugin"), 0, 20L);
     }
 
     public void removeCage(Player player, long delay, JavaPlugin plugin, Location playerLocation) {
@@ -93,12 +110,16 @@ public class Setup implements CommandExecutor
                 }
             }
         }.runTaskLater(plugin, delay * 20L);
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            player.setGameMode(GameMode.SURVIVAL);
+        }
     }
 
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public World generateWorld(CommandSender sender)
+    {
         if (!(sender instanceof Player)) {
-            sender.sendMessage("Cette commande ne peut être utilisée que par un joueur.");
-            return false;
+            sender.sendMessage(ChatColor.RED + "Cette commande ne peut être exécutée que par un joueur.");
+            return null;
         }
         Player player = (Player) sender;
         World newWorld = null;
@@ -106,44 +127,54 @@ public class Setup implements CommandExecutor
         long seed;
         WorldCreator worldCreator;
         Biome biomeAtSpawn;
+
+        player.sendTitle(ChatColor.YELLOW + "Loading map...", "");
         while (!acceptableBiome) {
             seed = new Random().nextLong();
             worldCreator = new WorldCreator("world_" + seed);
             worldCreator.seed(seed);
             newWorld = player.getServer().createWorld(worldCreator);
-            biomeAtSpawn = newWorld.getBiome(0, newWorld.getHighestBlockYAt(0, 0));
+            biomeAtSpawn = newWorld.getBiome(0, 0);
             if (biomeAtSpawn != Biome.OCEAN && biomeAtSpawn != Biome.RIVER && biomeAtSpawn != Biome.DEEP_OCEAN) {
                 acceptableBiome = true;
             } else {
                 player.getServer().unloadWorld(newWorld, false);
             }
         }
-        if (newWorld != null) {
-            Location spawnLocation = newWorld.getHighestBlockAt(newWorld.getSpawnLocation()).getLocation().add(0, 1, 0);
-            newWorld.setPVP(false);
-            for (Player p : Bukkit.getOnlinePlayers()) {
-                player.teleport(spawnLocation);
-                Location playerLocation = player.getLocation();
-                setupPlayer(p);
-                encasePlayer(p, Material.GLASS);
-                playerLocation.setX(playerLocation.getBlockX() + 0.5);
-                playerLocation.setY(playerLocation.getBlockY() + 1);
-                playerLocation.setZ(playerLocation.getBlockZ() + 0.5);
-                p.teleport(playerLocation);
-            }
+        player.sendTitle("", "");
+        return newWorld;
+    }
+
+    public void setDifficulty(Difficulty difficulty)
+    {
+        for (World world : Bukkit.getWorlds()) {
+            world.setDifficulty(difficulty);
         }
+    }
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+    {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Cette commande ne peut être utilisée que par un joueur.");
+            return false;
+        }
+        Player player = (Player) sender;
+        World newWorld = generateWorld(sender);
+        Location spawnLocation = newWorld.getHighestBlockAt(newWorld.getSpawnLocation()).getLocation().add(0, 1, 0);
+
+        newWorld.setPVP(false);
         for (Player p : Bukkit.getOnlinePlayers()) {
+            player.teleport(spawnLocation);
+            Location playerLocation = player.getLocation();
+            setupPlayer(p);
+            encasePlayer(p, Material.BARRIER);
+            playerLocation.setX(playerLocation.getBlockX() + 0.5);
+            playerLocation.setY(playerLocation.getBlockY() + 1);
+            playerLocation.setZ(playerLocation.getBlockZ() + 0.5);
+            p.teleport(playerLocation);
             showTimer(p);
         }
-        Location spawnLocation = newWorld.getHighestBlockAt(newWorld.getSpawnLocation()).getLocation().add(0, 1, 0);
         removeCage(player, 6L, (JavaPlugin) Bukkit.getPluginManager().getPlugin("plugin"), spawnLocation);
-        for (Player p : Bukkit.getOnlinePlayers()) {
-            player.setGameMode(org.bukkit.GameMode.SURVIVAL);
-        }
-        newWorld.setPVP(true);
-        seed = newWorld.getSeed();
-        World world = Bukkit.getWorld("world_" + seed);
-        world.setDifficulty(Difficulty.HARD);
+        setDifficulty(Difficulty.HARD);
         return true;
     }
 }
