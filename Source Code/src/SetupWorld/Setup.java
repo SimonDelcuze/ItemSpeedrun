a5befa670f8f539c8875de493f723c2832d64c8d
Simon Delcuze <simon.delcuze@epitech.eu>
2024-03-23 09:51:49 +0100

    [FEAT] Removing the cage around the player at the start

--- Source Code/src/SetupWorld/Setup.java
+++ Source Code/src/SetupWorld/Setup.java
@@ -1,32 +1,34 @@
 package SetupWorld;
 
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.WorldCreator;
+
 import java.util.Random;
-import org.bukkit.World;
-import org.bukkit.Location;
+
 import org.bukkit.block.Biome;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.scheduler.BukkitRunnable;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class Setup implements CommandExecutor
 {
 
-    //Setup the player's inventory and health
     public void setupPlayer(Player player)
     {
         player.getInventory().clear();
+
         player.setHealth(20);
         player.setFoodLevel(20);
         player.setSaturation(20);
-        player.setGameMode(org.bukkit.GameMode.SURVIVAL);
+        player.setGameMode(org.bukkit.GameMode.ADVENTURE);
         player.getInventory().clear();
         PlayerInventory inventory = player.getInventory();
         ItemStack item = new ItemStack(Material.COOKED_BEEF, 64);
@@ -34,23 +36,25 @@ public class Setup implements CommandExecutor
     }
 
     //Build an invisible box around the player
-    public void encasePlayer(Player player)
+    public void encasePlayer(Player player, Material material)
     {
         World world = player.getWorld();
         Location playerLocation = player.getLocation();
         playerLocation.setX(playerLocation.getBlockX() + 0.5);
         playerLocation.setY(playerLocation.getBlockY());
         playerLocation.setZ(playerLocation.getBlockZ() + 0.5);
-        int radius = 1;
+        int radius = 2;
 
         for (int x = -radius; x <= radius; x++) {
-            for (int y = 0; y <= 3; y++) {
+            for (int y = 0; y <= 4; y++) {
                 for (int z = -radius; z <= radius; z++) {
-                    if ((y == 1 || y == 2) && x == 0 && z == 0) {
+                    if ((y == 1 || y == 2 || y == 3) && (x == 0 || x == 1 || x == -1) && (z == 0 || z == 1 || z == -1)) {
+                        Location blockLocation = playerLocation.clone().add(x, y, z);
+                        world.getBlockAt(blockLocation).setType(Material.AIR);
                         continue;
                     }
                     Location blockLocation = playerLocation.clone().add(x, y, z);
-                    world.getBlockAt(blockLocation).setType(Material.BARRIER);
+                    world.getBlockAt(blockLocation).setType(material);
                 }
             }
         }
@@ -69,10 +73,28 @@ public class Setup implements CommandExecutor
                     cancel();
                 }
             }
-        }.runTaskTimer(Bukkit.getPluginManager().getPlugin("plugin"), 10, 20);
+        }.runTaskTimer(Bukkit.getPluginManager().getPlugin("plugin"), 10, 20L);
     }
 
-    // Load a new map for the player to play on
+    public void removeCage(Player player, long delay, JavaPlugin plugin, Location playerLocation) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                World world = player.getWorld();
+                int radius = 2;
+
+                for (int x = -radius; x <= radius; x++) {
+                    for (int y = 0; y <= 4; y++) {
+                        for (int z = -radius; z <= radius; z++) {
+                            Location blockLocation = playerLocation.clone().add(x, y, z);
+                            world.getBlockAt(blockLocation).setType(Material.AIR);
+                        }
+                    }
+                }
+            }
+        }.runTaskLater(plugin, delay * 20L);
+    }
+
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
             sender.sendMessage("Cette commande ne peut être utilisée que par un joueur.");
@@ -98,11 +120,12 @@ public class Setup implements CommandExecutor
         }
         if (newWorld != null) {
             Location spawnLocation = newWorld.getHighestBlockAt(newWorld.getSpawnLocation()).getLocation().add(0, 1, 0);
+            newWorld.setPVP(false);
             for (Player p : Bukkit.getOnlinePlayers()) {
                 player.teleport(spawnLocation);
                 Location playerLocation = player.getLocation();
                 setupPlayer(p);
-                encasePlayer(p);
+                encasePlayer(p, Material.GLASS);
                 playerLocation.setX(playerLocation.getBlockX() + 0.5);
                 playerLocation.setY(playerLocation.getBlockY() + 1);
                 playerLocation.setZ(playerLocation.getBlockZ() + 0.5);
@@ -112,7 +135,15 @@ public class Setup implements CommandExecutor
         for (Player p : Bukkit.getOnlinePlayers()) {
             showTimer(p);
         }
-        //removeCage(player);
+        Location spawnLocation = newWorld.getHighestBlockAt(newWorld.getSpawnLocation()).getLocation().add(0, 1, 0);
+        removeCage(player, 6L, (JavaPlugin) Bukkit.getPluginManager().getPlugin("plugin"), spawnLocation);
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            player.setGameMode(org.bukkit.GameMode.SURVIVAL);
+        }
+        newWorld.setPVP(true);
+        seed = newWorld.getSeed();
+        World world = Bukkit.getWorld("world_" + seed);
+        world.setDifficulty(Difficulty.HARD);
         return true;
     }
 }
